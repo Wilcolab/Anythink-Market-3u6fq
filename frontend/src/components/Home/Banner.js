48ea138c3a0d3259b3be687cc53eb09823a2763d
Devanshukoli <devanshukoli2@gmail.com>
2022-09-27 22:34:27 +0530

    modified ItemPreview and Banner files in js

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -5,10 +5,10 @@ const Banner = () => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
+        <img src={logo} alt="banner"/>
         <div>
           <span id="get-part">A place to get</span>
-          <span> the cool stuff.</span>
+          <span>the cool stuff.</span>
         </div>
       </div>
     </div>
