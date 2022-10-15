442a97ab52f84c028c6dadc395b5794e2b0f92d2
Devanshukoli <devanshukoli2@gmail.com>
2022-10-15 05:57:24 +0000

    Update Header.js file

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -49,25 +49,24 @@ const LoggedInView = (props) => {
   );
 };
 
-class Header extends React.Component {
-  render() {
-    return (
-      <nav
-        className="navbar navbar-expand-md navbar-dark"
-        style={{ padding: "0.5rem 2rem" }}
-      >
-        <Link to="/" className="navbar-brand">
-          <img alt="logo" src={logo} />
-        </Link>
+// Using Hooks...
+function Header() {
+  return (
+    <nav
+      className="navbar navbar-expand-md navbar-dark"
+      style={{ padding: "0.5rem 2rem" }}
+    >
+      <Link to="/" className="navbar-brand">
+        <img alt="logo" src={logo} />
+      </Link>
 
-        {this.props.currentUser ? (
-          <LoggedInView currentUser={this.props.currentUser} />
-        ) : (
-          <LoggedOutView currentUser={this.props.currentUser} />
-        )}
-      </nav>
-    );
-  }
+      {this.props.currentUser ? (
+        <LoggedInView currentUser={this.props.currentUser} />
+      ) : (
+        <LoggedOutView currentUser={this.props.currentUser} />
+      )}
+    </nav>
+  );
 }
 
 export default Header;
