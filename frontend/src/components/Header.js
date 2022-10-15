8beb2f723ea71b0c8ce94b8c9c54c46e7ac706de
Devanshukoli <devanshukoli2@gmail.com>
2022-10-15 08:43:54 +0000

    Update header.js file

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../imgs/topbar_logo.png";
-import React, {useState} from "react";
 
 const LoggedOutView = () => {
   return (
