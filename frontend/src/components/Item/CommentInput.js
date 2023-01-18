ec1c51445dda437a61246ac4e3c827ff7cb2cf2d
Devanshukoli <devanshukoli2@gmail.com>
2023-01-18 15:08:41 +0000

    added usestate into top of file

--- frontend/src/components/Item/CommentInput.js
+++ frontend/src/components/Item/CommentInput.js
@@ -1,5 +1,6 @@
 import React from "react";
 import agent from "../../agent";
+import React, { useState } from 'react';
 import { connect } from "react-redux";
 import { ADD_COMMENT } from "../../constants/actionTypes";
 
