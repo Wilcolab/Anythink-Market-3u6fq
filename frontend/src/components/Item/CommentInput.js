5b54414683441f02a2bf23ec081bd6c29d3e458b
Devanshukoli <devanshukoli2@gmail.com>
2023-01-18 15:33:48 +0000

    removed extra declaration of 'react'

--- frontend/src/components/Item/CommentInput.js
+++ frontend/src/components/Item/CommentInput.js
@@ -1,6 +1,5 @@
-import React from "react";
-import agent from "../../agent";
 import React, { useState } from 'react';
+import agent from "../../agent";
 import { connect } from "react-redux";
 import { ADD_COMMENT } from "../../constants/actionTypes";
 
