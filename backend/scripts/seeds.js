206353be35154fb3e940eb6ecbf3234e41d2b087
Devanshukoli <99028806+Devanshukoli@users.noreply.github.com>
2023-10-02 10:02:39 +0000

    <fix> : Again updated seeds.js file

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,62 +1,94 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
-// seeds.js 
 
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose')
 
-const url = 'mongodb://localhost:27017';
+require('../models/User')
+require('../models/Item')
+require('../models/Comment')
+
+var User = mongoose.model('User')
+var Item = mongoose.model('Item')
+var Comment = mongoose.model('Comment')
+
+// This helps to connect mongodb.
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI)
+} else {
+    console.warn('Missing MONGODB_URI in the env.')
+}
+
+let user_id
+
+let item_id
 
 async function seedDatabase() {
 
-    const client = new MongoClient(url);
+    // For users
+    const users = Array.from(Array(100)).map((_item, i) => ({
+        username: `testUser${i}`,
+        email: `testUser${i}@gmail.com`,
+        bio: `test bio`,
+        image: `https://images.unsplash.com/photo-1685032975515-6bd5815df416?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1785&q=80`,
+        role: 'user',
+        favorites: [],
+        following: [],
+    }))
 
-    try {
-        await client.connect();
+    for (let user of users) {
+        const u = new User(user)
 
-        const db = client.db('anythink');
 
-        // Generate 100 users
-        const users = [];
+        const dbItem = await u.save()
 
-        for (let i = 0; i < 100; i++) {
-            users.push({
-                name: `User ${i + 1}`,
-                email: `user${i + 1}@example.com`
-            });
+        if (!user_id) {
+            user_id = dbItem._id
         }
+    }
 
-        await db.collection('users').insertMany(users);
 
-        // Generate 100 products
-        const products = [];
+    // For Items.
+    const items = Array.from(Array(100)).map((_item, i) => ({
+        slug: `testItems${i}`,
+        title: `test title ${i}`,
+        description: `test Description`,
+        image: `https://images.unsplash.com/photo-1695576514502-cbc0a333f2d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1780&q=80`,
+        comments: [],
+        tagList: ['test', 'tag'],
+        seller: user_id
+    }))
 
-        for (let i = 0; i < 100; i++) {
-            products.push({
-                name: `Product ${i + 1}`,
-                description: 'Lorem ipsum dolor sit amet'
-            });
+    for (item of items) {
+        const it = new Item(item)
+
+        const dbItem = await it.save()
+
+        if (!item_id) {
+            item_id = dbItem._id
         }
+    }
 
-        await db.collection('products').insertMany(products);
 
-        // Generate 100 comments
-        const comments = [];
+    // For comments.
+    const comments = Array.from(Array(100)).map((_item, i) => ({
+        body: 'This is test coments.',
+        seller: user_id,
+        item: item_id
 
-        for (let i = 0; i < 100; i++) {
-            comments.push({
-                content: 'Lorem ipsum dolor sit amet',
-                user_id: Math.ceil(Math.random() * 100),
-                product_id: Math.ceil(Math.random() * 100)
-            });
-        }
+    }))
 
-        await db.collection('comments').insertMany(comments);
+    for (comment of comments) {
+        const c = new Comment(comment);
 
-    } catch (err) {
-        console.error(err);
-    } finally {
-        client.close();
+        await c.save()
     }
 
 }
 
-seedDatabase();
+
+seedDatabase().then(() => {
+    process.exit()
+}).catch((err) => {
+    console.error()
+
+    process.exit()
+})
