0210c92a23d7fbeebb970a403caa44b2b2ae40da
WilcoApp <hello@wilco.gg>
2023-10-02 08:46:50 +0000

    Initial commit

--- frontend/src/components/Item/CommentInput.js
+++ frontend/src/components/Item/CommentInput.js
@@ -7,42 +7,53 @@ const mapDispatchToProps = (dispatch) => ({
   onSubmit: (payload) => dispatch({ type: ADD_COMMENT, payload }),
 });
 
-const CommentInput = (props) => {
-  const [body, setBody] = React.useState("");
+class CommentInput extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      body: "",
+    };
 
-  const createComment = async (ev) => {
-    ev.preventDefault();
-    agent.Comments.create(props.slug, {
-      body: body,
-    }).then((payload) => {
-      props.onSubmit(payload);
-    });
-    setBody("");
-  };
+    this.setBody = (ev) => {
+      this.setState({ body: ev.target.value });
+    };
 
-  return (
-    <form className="card comment-form m-2" onSubmit={createComment}>
-      <div className="card-block">
-        <textarea
-          className="form-control"
-          placeholder="Write a comment..."
-          value={body}
-          onChange={(ev) => setBody(ev.target.value)}
-          rows="3"
-        ></textarea>
-      </div>
-      <div className="card-footer">
-        <img
-          src={props.currentUser.image}
-          className="user-pic mr-2"
-          alt={props.currentUser.username}
-        />
-        <button className="btn btn-sm btn-primary" type="submit">
-          Post Comment
-        </button>
-      </div>
-    </form>
-  );
-};
+    this.createComment = async (ev) => {
+      ev.preventDefault();
+      agent.Comments.create(this.props.slug, {
+        body: this.state.body,
+      }).then((payload) => {
+        this.props.onSubmit(payload);
+      });
+      this.setState({ body: "" });
+    };
+  }
+
+  render() {
+    return (
+      <form className="card comment-form m-2" onSubmit={this.createComment}>
+        <div className="card-block">
+          <textarea
+            className="form-control"
+            placeholder="Write a comment..."
+            value={this.state.body}
+            onChange={this.setBody}
+            rows="3"
+          ></textarea>
+        </div>
+        <div className="card-footer">
+          <img
+            src={this.props.currentUser.image}
+            className="user-pic mr-2"
+            alt={this.props.currentUser.username}
+          />
+          <button className="btn btn-sm btn-primary" type="submit">
+            Post Comment
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
 
 export default connect(() => ({}), mapDispatchToProps)(CommentInput);
